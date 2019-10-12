9572bc9543f3ec7a2c1cd9c274f950ded807e71d
letniqq <letniqq@localhost.localdomain>
2019-10-12 03:48:46 +0300

    mongo starter + celery

--- app/api/api_v1/endpoints/stats.py
+++ app/api/api_v1/endpoints/stats.py
@@ -1,17 +1,22 @@
 import requests
-from typing import List
+from typing import List, Optional
 import json
 from fastapi import APIRouter, Depends, HTTPException
 from app.db_models.user import User as DBUser
 from app.api.utils.security import get_current_active_user
-from app.models.stats import Stats
+from app.crud.exchange import insert_if_not_exist
+from app.core.utils import create_aliased_response, date_to_isodate
+from app.mongo.mongodb import AsyncIOMotorClient, get_db
 
 
 router = APIRouter()
-@router.get('/stats/')
-def get_stats(date: int = None,
-              current_user: DBUser = Depends(get_current_active_user)
-) -> List:
+
+
+@router.get('/exchange/')
+async def get_exchange(date: Optional[int] = None,
+                       db: AsyncIOMotorClient = Depends(get_db),
+                       current_user: DBUser = Depends(get_current_active_user)
+                       ):
     """
     Get stats for today
     """
@@ -20,4 +25,8 @@ def get_stats(date: int = None,
         url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date={}&json'.format(date)
     r = requests.get(url)
     stats = r.json()
-    return stats
+    for stat in stats:
+        stat['exchangedate'] = date_to_isodate(stat['exchangedate'])
+        await insert_if_not_exist(db, stat)
+
+
